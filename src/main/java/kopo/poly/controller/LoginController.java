cedbd3d06bf1a464919d9bb834ab2c8228351658
psb <kongsuni3685@gmail.com>
2023-10-28 17:55:30 +0900

    231028 박세빈 로그아웃 구현 완료 + index 페이지 네비게이션 메뉴 일부 수정 + 네이버 로그인, 카카오 로그인 구현 중

--- src/main/java/kopo/poly/controller/LoginController.java
+++ src/main/java/kopo/poly/controller/LoginController.java
@@ -2,16 +2,12 @@ package kopo.poly.controller;
 
 import kopo.poly.dto.UserInfoDTO;
 import kopo.poly.service.ILoginService;
-import kopo.poly.service.ISignupService;
 import kopo.poly.util.CmmUtil;
 import kopo.poly.util.EncryptUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -33,6 +29,48 @@ public class LoginController {
         log.info(this.getClass().getName() + ".login End!");
 
         return "/user/login";
+    }
+
+    @GetMapping(value = "/naverLogin")
+    public String naverLogin() {
+
+        log.info(this.getClass().getName() + ".naverLogin Loading Complete!");
+
+        return "/user/naverLogin";
+    }
+
+    @GetMapping(value = "/naverLogin/callBack")
+    public String naverCallBack() {
+
+        log.info(this.getClass().getName() + ".naverCallBack Start!");
+        log.info(this.getClass().getName() + ".naverCallBack End!");
+
+        return "/user/naverLogin_callback";
+    }
+
+    @RequestMapping(value = "/kakaoLogin")
+    public String kakaoLogin(@RequestParam("code") String code, HttpSession session) throws Exception {
+
+        log.info(this.getClass().getName() + ".kakaoLogin Start!");
+
+        String access_Token = loginService.getAccessToken(code);
+        HashMap<String, Object> userInfo = loginService.getUserInfo(access_Token);
+        System.out.println("login Controller : " + userInfo);
+
+        //    클라이언트의 이메일이 존재할 때 세션에 해당 이메일과 토큰 등록
+        if (userInfo.get("email") != null) {
+            // session.setAttribute("userId", userInfo.get("email"));
+            session.setAttribute("SS_USER_ID", userInfo.get("email"));
+            session.setAttribute("access_Token", access_Token);
+        }
+
+        log.info("code : " + code);
+        log.info("access_Token : " + access_Token);
+        log.info("userInfo.get(\"email\") : " + userInfo.get("email"));
+
+        log.info(this.getClass().getName() + ".kakaoLogin End!");
+
+        return "/index";
     }
 
     @PostMapping(value = "/loginProc")
@@ -74,6 +112,7 @@ public class LoginController {
 
                 log.info("SS_USER_ID : " + userId);
                 session.setAttribute("SS_USER_ID", userId);
+                log.info("세션에 저장 후 session.getAttribute(\"SS_USER_ID\") : " + session.getAttribute("SS_USER_ID"));
 
                 msg = "로그인 성공! " + rDTO.getUserName() + "님 환영합니다.";
                 url = "/index";
@@ -98,5 +137,34 @@ public class LoginController {
 
         return response;
     }
+
+
+
+
+    @RequestMapping(value="/logout")
+    public String logout(HttpSession session) throws Exception {
+
+        log.info(this.getClass().getName() + ".logout Start!");
+
+        // 만약 access_Token이 존재하면, 카카오 로그아웃 메소드 호출하기!
+        if (session.getAttribute("access_Token") != null) {
+
+            // 카카오 로그아웃 메소드 호출
+            loginService.kakaoLogout((String) session.getAttribute("access_Token"));
+
+            // 세션에 있는 접근토큰 삭제하기!
+            session.removeAttribute("access_Token");
+        }
+
+        // 세션에 있는 유저아이디 삭제하기!
+        session.removeAttribute("SS_USER_ID");
+
+        log.info("세션 삭제 후 session.getAttribute(\"SS_USER_ID\") : " + session.getAttribute("SS_USER_ID"));
+
+        log.info(this.getClass().getName() + ".logout End!");
+
+        return "/user/login";
+    }
+
 
 }
--- src/main/java/kopo/poly/service/ILoginService.java
+++ src/main/java/kopo/poly/service/ILoginService.java
@@ -3,8 +3,18 @@ package kopo.poly.service;
 
 import kopo.poly.dto.UserInfoDTO;
 
+import java.util.HashMap;
+
 public interface ILoginService {
 
     // 로그인 시 아이디, 비밀번호 일치하는지 확인하기
     UserInfoDTO getLogin(UserInfoDTO pDTO) throws Exception;
+
+    // 카카오 로그인
+    String getAccessToken(String authorize_code) throws Exception;
+    HashMap<String, Object> getUserInfo (String access_Token) throws Exception;
+
+    // 카카오 로그아웃
+    void kakaoLogout(String access_Token) throws Exception;
+
 }
