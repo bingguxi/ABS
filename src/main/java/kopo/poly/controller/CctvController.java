3d1bd628dbf0ae43a3a3dfd9fe9fb3dc34676234
chna8137 <chna8137s@gmail.com>
2023-11-14 22:14:29 +0900

    231114 cctv api 구현 중

--- src/main/java/kopo/poly/controller/CctvController.java
+++ src/main/java/kopo/poly/controller/CctvController.java
@@ -1,18 +1,26 @@
 package kopo.poly.controller;
 
+import kopo.poly.dto.CctvResultDTO;
 import kopo.poly.service.impl.CctvService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Slf4j
-@Controller
+@RequiredArgsConstructor
+@RequestMapping("cctv")
+@RestController
 public class CctvController {
 
-    @Autowired
-    private CctvService cctvService;
+//    @Autowired
+    private final CctvService cctvService;
 
     @GetMapping("/findCctv")
     public String findCctv(Model model) {
@@ -29,5 +37,45 @@ public class CctvController {
         log.info(this.getClass().getName() + ".findCctv End!");
 
         return "cctvResult";
+    }
+
+    @ResponseBody
+    @GetMapping("getCctv")
+    public List<CctvResultDTO> getCctv(HttpServletRequest request) throws Exception {
+
+        log.info(this.getClass().getName() + ".getCctv Start!!");
+
+        List<CctvResultDTO> rList = cctvService.getCctv();
+
+        log.info("rList : ", rList);
+
+        for (CctvResultDTO rDTO : rList) {
+            log.info("-------------------------------");
+            log.info("날짜 : " + rDTO.getCctvName());
+            log.info("국내 지점번호 : " + rDTO.getCctvUrl());
+            log.info("지점명 : " + rDTO.getCoordX());
+            log.info("경도 : " + rDTO.getCoordY());
+        }
+
+//        String type = CmmUtil.nvl(request.getParameter("type")); // 도로 유형(ex: 고속도로 / its: 국도)
+//        String CctvType = CmmUtil.nvl(request.getParameter("CctvType"));  // CCTV 유형(1: 실시간 스트리밍(HLS) / 2: 동영상 파일 / 3: 정지 영상)
+//        String MinX = CmmUtil.nvl(request.getParameter("MinX")); // 최소 경도 영역
+//        String MaxX = CmmUtil.nvl(request.getParameter("MaxX")); // 최대 경도 영역
+//        String MinY = CmmUtil.nvl(request.getParameter("MinY")); // 최소 위도 영역
+//        String MaxY = CmmUtil.nvl(request.getParameter("MaxY")); // 최대 위도 영역
+//        String GetType = CmmUtil.nvl(request.getParameter("GetType")); // 출력 결과 형식(xml, json / 기본: xml)
+//
+//        pDTO.setType(type);
+//        pDTO.setCctvType(CctvType);
+//        pDTO.setMinX(MinX);
+//        pDTO.setMaxX(MaxX);
+//        pDTO.setMinY(MinY);
+//        pDTO.setMaxY(MaxY);
+//        pDTO.setGetType(GetType);
+
+
+//        List<CctvResultDTO> rList = cctvService.getCctv(pDTO);
+
+        return rList;
     }
 }
--- src/main/java/kopo/poly/service/impl/CctvService.java
+++ src/main/java/kopo/poly/service/impl/CctvService.java
@@ -1,16 +1,104 @@
 package kopo.poly.service.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kopo.poly.dto.CctvDTO;
+import kopo.poly.dto.CctvResultDTO;
+import kopo.poly.persistance.mapper.ICctvMapper;
 import kopo.poly.service.ICctvService;
+import kopo.poly.util.CmmUtil;
+import kopo.poly.util.NetworkUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.*;
+
+@Slf4j
+@RequiredArgsConstructor
 @Service
 public class CctvService implements ICctvService {
 
+    private final ICctvMapper cctvMapper;
+
     @Value("${cctv.api.key}")
-    private String apiKey; // application.properties 또는 application.yml 파일에서 API 키 설정
+    private String apiKey1; // application.properties 또는 application.yml 파일에서 API 키 설정
+
+
+    @Value("${cctvResult.api.key}")
+    private String apiKey;
+
+    @Override
+    public List<CctvResultDTO> getCctv() throws Exception {
+
+        log.info(this.getClass().getName() + ".getCctv Start!!");
+
+        CctvDTO pDTO = new CctvDTO();
+
+
+        String type = CmmUtil.nvl(pDTO.getType());
+        String cctvType = CmmUtil.nvl(pDTO.getCctvType());
+        String minX = CmmUtil.nvl(pDTO.getMinX());
+        String maxX = CmmUtil.nvl(pDTO.getMaxX());
+        String minY = CmmUtil.nvl(pDTO.getMinY());
+        String maxY = CmmUtil.nvl(pDTO.getMaxY());
+        String getType = CmmUtil.nvl(pDTO.getGetType());
+
+        String apiParam = url + "?apiKey=" + apiKey + "&type=" + type + "&cctvType=" + cctvType + "&minX=" + minX + "&maxX=" + maxX + "&minY=" + minY + "&maxY=" + maxY + "&getType=" + getType;
+        log.info("apiParam : " + apiParam);
+
+        Map<String, String> headers = new HashMap<>();
+//        headers.put("Content-Type", "json");
+
+        String json = NetworkUtil.get(apiParam, headers);
+//        log.info("json" + json);
+
+
+        Map<String, Object> rMap = new ObjectMapper().readValue(json, LinkedHashMap.class);
+
+        // CCTV의 정보를 가지고 있는 data 키의 값 가져오기
+        Map<String, Object> dMap = (Map<String, Object>) rMap.get("response");
+
+        // 일별 날씨 조회(OpenAPI가 현재 날짜 기준으로 최대 7일까지 제공)
+        List<Map<String, Object>> dataList = (List<Map<String, Object>>) dMap.get("data");
+//        log.info("dataList.size() : " + dataList.size());
+
+        // OpenAPI로부터 필요한 정보만 가져와서, 처리하기 쉬운 JSON 구조로 변경에 활용
+        List<CctvResultDTO> pList = new LinkedList<>();
+
+        for (Map<String, Object> dataMap : dataList) {
+            CctvResultDTO rDTO = new CctvResultDTO();
+
+            rDTO.setCoordX(
+                    (Number) dataMap.get("coordx")
+            );
+            rDTO.setCoordY(
+                    (Number) dataMap.get("coordy")
+            );
+            // FIXME api json 키 값은 cctvname (카멜 케이스가 아닌데)
+            //  dto에서는 카멜식임
+            rDTO.setCctvName(
+                    (String) dataMap.get("cctvname")
+            );
+            rDTO.setCctvUrl(
+                    (String) dataMap.get("cctvurl")
+            );
+
+//            log.info("rDTO : " + rDTO.toString());
+
+            cctvMapper.insertCctvInfo(rDTO);
+
+            // TODO 얘들이 정상적인 dto인지 확인하고 넣으면 좀더 좋은
+            pList.add(rDTO);
+        }
+        List<CctvResultDTO> rList = cctvMapper.getCctv();
+
+        log.info(this.getClass().getName() + ".getCctv End!!");
+
+        return rList;
+    }
 
     // CCTV 정보 가져오기
     @Override
@@ -24,7 +112,7 @@ public class CctvService implements ICctvService {
 
             // API 호출 URL 생성
             String apiUrl = "https://openapi.its.go.kr:9443/cctvInfo?"
-                    + "apiKey=" + apiKey
+                    + "apiKey1=" + apiKey1
                     + "&type=ex&cctvType=2"
                     + "&minX=" + minX
                     + "&maxX=" + maxX
