89af5d28e2a8d9a95bf358306b1478622a428d15
psb <kongsuni3685@gmail.com>
2023-11-24 17:10:13 +0900

    231124 박세빈 CCTV API 개선

--- src/main/java/kopo/poly/service/ICctvService.java
+++ src/main/java/kopo/poly/service/ICctvService.java
@@ -6,16 +6,9 @@ import java.util.List;
 
 public interface ICctvService {
 
-    String apiURL = "https://openapi.its.go.kr:9443/cctvInfo?apiKey=test&type=ex&cctvType=1&minX=127.100000&maxX=128.890000&minY=34.100000&maxY=39.100000&getType=json";
-
     String url = "https://openapi.its.go.kr:9443/cctvInfo";
-
 
     // cctv api호출하여 cctv 결과 받아오기
     List<CctvResultDTO> getCctv() throws Exception;
 
-
-
-    // CCTV 정보 가져오기
-    String getCctvData(double lat, double lng);
 }
