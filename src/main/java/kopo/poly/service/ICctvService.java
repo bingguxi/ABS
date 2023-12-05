3ca61dc95b64fbf8cf5a47a605dd381dc64dadee
psb <kongsuni3685@gmail.com>
2023-12-05 14:04:08 +0900

    231205 박세빈 스케줄링 프로젝트 분리 작업 중 (CCTV 완료)

--- src/main/java/kopo/poly/service/ICctvService.java
+++ src/main/java/kopo/poly/service/ICctvService.java
@@ -8,9 +8,6 @@ public interface ICctvService {
 
     String url = "https://openapi.its.go.kr:9443/cctvInfo";
 
-    // 수집된 cctv 정보 DB에 등록
-    void insertCctvInfo(CctvResultDTO pDTO) throws Exception;
-
     // DB에 저장된 cctv 조회하기
     List<CctvResultDTO> getCctv() throws Exception;
 
--- src/main/java/kopo/poly/service/impl/CctvService.java
+++ src/main/java/kopo/poly/service/impl/CctvService.java
@@ -23,56 +23,6 @@ public class CctvService implements ICctvService {
     private String apiKey;
 
     @Override
-    public void insertCctvInfo(CctvResultDTO pDTO) throws Exception {
-
-        log.info(this.getClass().getName() + ".insertCctvInfo Start!!");
-
-        log.info("DB 삭제 시작");
-
-        cctvMapper.deleteCctvInfo();
-
-        String type = "ex";
-        String cctvType = "1";
-        String minX = "127.100000";
-        String maxX = "128.890000";
-        String minY = "34.100000";
-        String maxY = "39.100000";
-        String getType = "json";
-
-        String apiParam = url + "?apiKey=" + apiKey + "&type=" + type + "&cctvType=" + cctvType + "&minX=" + minX + "&maxX=" + maxX + "&minY=" + minY + "&maxY=" + maxY + "&getType=" + getType;
-        log.info("apiParam : " + apiParam);
-
-        Map<String, String> headers = new HashMap<>();
-
-        String json = NetworkUtil.get(apiParam, headers);
-
-        Map<String, Object> rMap = new ObjectMapper().readValue(json, LinkedHashMap.class);
-
-        // CCTV의 정보를 가지고 있는 data 키의 값 가져오기
-        Map<String, Object> dMap = (Map<String, Object>) rMap.get("response");
-
-        // 일별 날씨 조회(OpenAPI가 현재 날짜 기준으로 최대 7일까지 제공)
-        List<Map<String, Object>> dataList = (List<Map<String, Object>>) dMap.get("data");
-
-        for (Map<String, Object> dataMap : dataList) {
-            CctvResultDTO rDTO = new CctvResultDTO();
-
-            // FIXME api json 키 값은 cctvname (카멜 케이스가 아닌데)
-            //  dto에서는 카멜식임
-            rDTO.setCoordX(String.valueOf(dataMap.get("coordx")));
-            rDTO.setCoordY(String.valueOf(dataMap.get("coordy")));
-            rDTO.setCctvName((String) dataMap.get("cctvname"));
-            rDTO.setCctvUrl((String) dataMap.get("cctvurl"));
-
-            cctvMapper.insertCctvInfo(rDTO);
-
-        }
-
-        log.info(this.getClass().getName() + ".insertCctvInfo End!!");
-
-    }
-
-    @Override
     public List<CctvResultDTO> getCctv() throws Exception {
 
         log.info(this.getClass().getName() + ".getCctv Start!");
