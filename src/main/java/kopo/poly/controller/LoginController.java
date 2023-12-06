3bef5417dca9652f30fdf7e2ece08a99b216a88f
bhg <kkoowl99@gmail.com>
2023-12-07 05:17:24 +0900

    231206 변형근,권은아 메인페이지 ( 재난 카운트, 메인페이지 css + 유튜브 디자인 ) + 각 재난 리스트 ( 조회 + 디자인 ) 완료 / [ 리스트 페이지 null값 처리 + 페이징 수정 필요 ]

--- src/main/java/kopo/poly/controller/LoginController.java
+++ src/main/java/kopo/poly/controller/LoginController.java
@@ -1,9 +1,10 @@
 package kopo.poly.controller;
 
-import kopo.poly.dto.DisasterMsgResultDTO;
-import kopo.poly.dto.UserInfoDTO;
+import kopo.poly.dto.*;
 import kopo.poly.service.IDisasterMsgService;
+import kopo.poly.service.IEarthquakeService;
 import kopo.poly.service.ILoginService;
+import kopo.poly.service.ITyphoonService;
 import kopo.poly.util.CmmUtil;
 import kopo.poly.util.EncryptUtil;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +26,10 @@ public class LoginController {
     private final ILoginService loginService;
 
     private final IDisasterMsgService disasterMsgService;
+
+    private final IEarthquakeService earthquakeService;
+
+    private final ITyphoonService typhoonService;
 
     @GetMapping("")
     public String login() {
@@ -225,7 +230,7 @@ public class LoginController {
 
         log.info(this.getClass().getName() + ".logout Start!");
 
-        /* 재난문자 리스트 슬라이드 로직 */
+        /* --------------------- 재난문자 리스트 슬라이드 로직 -------------------------- */
         // 서비스 메서드를 호출하여 데이터를 가져옴
         List<DisasterMsgResultDTO> last3List = disasterMsgService.getLast3DisasterMsgList();
 
@@ -234,7 +239,70 @@ public class LoginController {
 
         // 들어온 값 확인
         log.info("rList : " + last3List);
-        /* 여기까지 */
+        /* ------------------------------- 끝 ----------------------------------- */
+
+        /* ------------------------------------------------------------------------- */
+        /* 지진 + 태풍 실시간 정보 */
+        List<EarthquakeLiveDTO> earthquakeLiveTotal = earthquakeService.getEarthquakeLiveList();
+        List<TyphoonLiveDTO> typhoonLiveTotal = typhoonService.getTyphoonLiveList();
+
+        int elSize = earthquakeLiveTotal.size();
+        int tlSize = typhoonLiveTotal.size();
+
+        model.addAttribute("elSize", elSize); // 실시간 지진 카운트를 위해 배열의 사이즈를 재서 모델로 저장
+        model.addAttribute("tlSize", tlSize);
+
+        log.info("elSize ( 실시간 지진 ) : " + elSize);
+        log.info("tlSize ( 실시간 태풍 ) : " + tlSize);
+        /**/
+
+        /* 지진 태풍 올해 총 건수 정보 */
+        List<EarthquakeResultDTO> earthquakeTotal = earthquakeService.getEarthquakeList();
+        List<TyphoonDTO> typhoonTotal = typhoonService.getTyphoonList();
+
+        // tm_Issue 컬럼에 2023으로 시작하는 경우 필터링
+        long eTyCount = earthquakeTotal.stream()
+                .filter(dto -> dto.getTmIssue() != null && dto.getTmIssue().startsWith("2023"))
+                .count();
+
+        // year 컬럼에 2023으로 시작하는 경우 필터링
+        long tTyCount = typhoonTotal.stream()
+                .filter(dto -> dto.getYear() != null && dto.getYear().startsWith("2023"))
+                .count();
+
+        model.addAttribute("eTyCount", eTyCount);
+        model.addAttribute("tTyCount", tTyCount);
+
+        log.info("eTyCount ( 올해 지진 건수 ) : " + eTyCount);
+        log.info("tTyCount ( 올해 태풍 건수 ) : " + tTyCount);
+        /**/
+
+        /* 지진 태풍 작년 총 건수 정보 */
+        // tm_Issue 컬럼에 2023으로 시작하는 경우 필터링
+        long eLyCount = earthquakeTotal.stream()
+                .filter(dto -> dto.getTmIssue() != null && dto.getTmIssue().startsWith("2022"))
+                .count();
+
+        // year 컬럼에 2023으로 시작하는 경우 필터링
+        long tLyCount = typhoonTotal.stream()
+                .filter(dto -> dto.getYear() != null && dto.getYear().startsWith("2022"))
+                .count();
+
+
+        model.addAttribute("eLyCount", eLyCount);
+        model.addAttribute("tLyCount", tLyCount);
+
+        log.info("eLyCount ( 지난해 지진 건수 ) : " + eLyCount);
+        log.info("tLyCount ( 지난해 태풍 건수 ) : " + tLyCount);
+        /**/
+
+        // 올해 두 카운트를 합쳐서 모델에 추가
+        model.addAttribute("totalTyCount", eTyCount + tTyCount);
+
+        // 작년 두 카운트를 합쳐서 모델에 추가
+        model.addAttribute("totalLyCount", eLyCount + tLyCount);
+
+        /* ------------------------------------------------------------------------- */
 
         // 만약 access_Token이 존재하면, 카카오 로그아웃 메소드 호출하기!
         if (session.getAttribute("access_Token") != null) {
